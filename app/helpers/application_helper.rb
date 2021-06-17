ae49fab7881131fbad334285a4cad1c42c550094
Sokratis Vidros <sokratis.vidros@gmail.com>
2021-06-17 11:42:33 +0300

    Fetch latest prod Clerk JS

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def clerk_script_tag
     javascript_include_tag(
-      "https://#{ENV.fetch('CLERK_FRONTEND_API')}/npm/@clerk/clerk-js@staging/dist/clerk.browser.js",
+      "https://#{ENV.fetch('CLERK_FRONTEND_API')}/npm/@clerk/clerk-js@1/dist/clerk.browser.js",
       {
         'data-clerk-frontend-api': ENV.fetch('CLERK_FRONTEND_API'),
         crossorigin: 'anonymous',
